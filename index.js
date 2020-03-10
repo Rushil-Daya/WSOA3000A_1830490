76cd62041aa5cb15ba48e57187d34ceb69481761
Rushil-Daya <60914130+Rushil-Daya@users.noreply.github.com>
2020-03-10 09:22:16 +0200

    new folder fro blogs

--- index.js
+++ index.js
@@ -14,27 +14,16 @@ buttonPressed.addEventListener('click' , buttonPressed);
 
 const button = document.body.children[0];
 console.log(button);
-console.log('kl');
+console.log('kkklkll');
 
 function random(number) {
     return Math.floor(Math.random() * (number+1));
 }
 
-
-
-
-
 button.addEventListener ('click', function(e)   
 {
-    
-
     const colour = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
     document.body.style.backgroundColor= colour;
-    
-
-
-    console.log('pressed');
-
-
+        console.log('pressed');
 } 
 );
