5b32998f39c4961a9573f958fa198b026625969c
Rushil-Daya <60914130+Rushil-Daya@users.noreply.github.com>
2020-06-28 00:26:27 +0200

    additiona jquery

--- Resources/JavascriptFunctionality/JQueryFunctions.js
+++ Resources/JavascriptFunctionality/JQueryFunctions.js
@@ -1,8 +1,30 @@
-$(document).ready(function(){
-    $("#input").on("keyup",function(){
-        var inputValue = $(this).val().toLowerCase();
-        $("#poemLists li a").filter(function(){
+ $(document).ready(function(){
+     $("#input").on("keyup",function(){
+         var inputValue = $(this).val().toLowerCase();
+
+         
+         $("#poemLists li a").filter(function(){
             $(this).toggle($(this).text().toLowerCase().indexOf(inputValue) > -1)
-        });
-    });
-});
+
+           
+           
+         })
+     });
+ });
+
+// $(document).ready(function(){
+//     $("#input").on("keyup",function(){
+//         var inputValue = $(this).val().toLowerCase();
+
+//        $("a[href*= inputValue]").css("color", "red");
+           
+      
+//     });
+// });
+
+
+// $(this).toggle($(this).text().toLowerCase().indexOf(inputValue) > -1)
+
+// return $(this).text().toLowerCase().indexOf(inputValue) > -1;
+
+// .css("color", "red");
