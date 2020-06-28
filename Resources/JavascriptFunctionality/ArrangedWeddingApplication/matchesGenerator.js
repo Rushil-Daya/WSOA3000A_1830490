769615b54dd20a95cba78ed4749fc48619a93c43
Rushil-Daya <60914130+Rushil-Daya@users.noreply.github.com>
2020-06-28 14:23:01 +0200

    basics match generator done

--- Resources/JavascriptFunctionality/ArrangedWeddingApplication/matchesGenerator.js
+++ Resources/JavascriptFunctionality/ArrangedWeddingApplication/matchesGenerator.js
@@ -1,5 +1,13 @@
-
-
-  fetch('https://randomuser.me/api/?gender=female')
+fetch('https://randomuser.me/api/?results=3&gender=female&inc=name,gender,email,picture')
     .then((x) => x.json())
-    .then((response) => console.log(response))
+    .then((response) => {
+        
+        console.log(response);
+        resultAnalyser(response);
+    
+    });
+
+const resultAnalyser = (result) => {
+    document.querySelector('span').innerText= result.results[0].name.first;
+    document.querySelector('img').src = result.results[0].picture.large;
+}
